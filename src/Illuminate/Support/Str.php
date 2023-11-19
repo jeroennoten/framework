365ac4521e3332490eeb0deec42611be6f2cffde
Jeroen Noten <jeroennoten@me.com>
2023-11-19 19:54:40 +0100

    cat

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -459,7 +459,7 @@ class Str
     public static function startsWith($haystack, $needles)
     {
         foreach ((array) $needles as $needle) {
-            if ($needle != '' && substr($haystack, 0, strlen($needle)) === (string) $needle) {
+            if ($needle != '' && substr((string) $haystack, 0, strlen($needle)) === (string) $needle) {
                 return true;
             }
         }
