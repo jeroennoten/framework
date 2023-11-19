959408cb89ae4ea38a58f521cf9f46b7deacb85d
Jeroen Noten <jeroennoten@me.com>
2023-11-19 17:51:31 +0100

    cast

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -544,7 +544,7 @@ if (! function_exists('e')) {
             return $value->toHtml();
         }
 
-        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8', false);
+        return htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8', false);
     }
 }
 
